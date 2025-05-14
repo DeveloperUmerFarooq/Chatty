c3709a93225333c29ef6ba66e462a2b0efbc1eae
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-14 17:30:45 +0500

    Fixed deployement error

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@ import path from "path"
 
 dotenv.config()
 
+const __dirname=path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -25,15 +26,14 @@ app.use("/api/auth",authRoutes);
 //message routes
 app.use("/api/messages",messageRoutes);
 
-
 //Start Server
 const PORT= process.env.PORT
 // Production deployement setting start
-const __dirname=path.resolve();
+
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("*",(req,res)=>{
+    app.get("/",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
 }
