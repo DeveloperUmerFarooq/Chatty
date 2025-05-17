3cdb374fedd5e815956d75e0d8c3aeb716b586bb
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-05-17 21:26:31 +0500

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { app,server } from './lib/socket.js'
+import path from "path"
 
 dotenv.config()
-
+const dirname=path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -24,6 +25,13 @@ app.use("/api/auth",authRoutes);
 //message routes
 app.use("/api/messages",messageRoutes);
 
+if(process.env.NODE_ENV==="production"){
+
+    app.use(express.static(path.join(dirname,"../frontend/dist")));
+    app.get("/",(req,res)=>{
+        res.sendFile(path.join(dirname,"../frontend/dist/index.html"))
+    })
+}
 //Start Server
 const PORT= process.env.PORT
 server.listen(PORT,()=>{
